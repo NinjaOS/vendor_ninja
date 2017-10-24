110d327151945dd333db981b9f211f52400c7736
iamsj7 <shaikjaleel270@gmail.com>
2017-10-24 15:41:57 +0000

    fix shit

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -3,7 +3,7 @@ ninja_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
-	echo '"Jdc": {'; \
+	echo '"ninja": {'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
 	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false)'; \
 	echo '},'; \
