nychitman1/android_vendor_du

Fix AOSP gesture typing
Change-Id: Ia19043df237f41ebe0ff556fe794eb115d6ec303

config/common.mk
@@ -33,7 +33,9 @@ PRODUCT_COPY_FILES += \
 ifeq ($(TARGET_ARCH),arm64)
 PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
-    vendor/du/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
+    vendor/du/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so \
+    vendor/du/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+    vendor/du/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 else
 PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \

NinjaOS/vendor_ninja

config/common.mk
@@ -88,14 +88,14 @@ PRODUCT_COPY_FILES += \
     vendor/ninja/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner
 
 # LatinIME gesture typing
-ifeq ($(TARGET_ARCH),arm64)
-PRODUCT_COPY_FILES += \
-    vendor/ninja/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
-    vendor/ninja/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
-else
+ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES += \
     vendor/ninja/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
     vendor/ninja/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/ninja/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/ninja/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 endif
 
 # NinjOS-specific init file
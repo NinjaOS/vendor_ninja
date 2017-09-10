8f66dd78e274b337901b520a22c761f099de3980
Josh Chasky <jchasky@gmail.com>
2017-09-10 12:41:51 -0400

    Fix AOSP gesture typing
    Change-Id: Ia19043df237f41ebe0ff556fe794eb115d6ec303

--- config/common.mk
+++ config/common.mk
@@ -88,14 +88,14 @@ PRODUCT_COPY_FILES += \
     vendor/ninja/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner
 
 # LatinIME gesture typing
-ifeq ($(TARGET_ARCH),arm64)
-PRODUCT_COPY_FILES += \
-    vendor/ninja/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
-    vendor/ninja/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
-else
+ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES += \
     vendor/ninja/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
     vendor/ninja/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/ninja/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/ninja/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 endif
 
 # NinjOS-specific init file
