c70f00c2e1b7877d69c1c6881eea467c65697a56
nsytechno <nsytechno@gmail.com>
2017-11-13 18:21:59 +0530

    fixup

--- google/GoogleAudio.mk
+++ google/GoogleAudio.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := vendor/du/google
+LOCAL_PATH := vendor/ninja/google
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/alarms/Argon.ogg:system/media/audio/alarms/Argon.ogg \
